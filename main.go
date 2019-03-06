6fe12796c8521a52912c8742dfa50820c0b5ee1e
biohuns <18520474+biohuns@users.noreply.github.com>
2019-03-07 01:53:04 +0900

    シンプルなコマンドラインツールに変更

--- main.go
+++ main.go
@@ -1,18 +1,48 @@
 package main
 
 import (
-	"log"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net"
+	"os"
 
-	"github.com/biohuns/go-minecraft-stats/config"
-	"github.com/biohuns/go-minecraft-stats/server"
+	"github.com/andrewtian/minepong"
 )
 
 func main() {
-	// Load Config
-	cfg, err := config.LoadConfig()
-	if err != nil {
-		log.Fatalln(err)
+	var (
+		host = flag.String("host", "", "hostname string")
+		port = flag.Int("port", 25565, "port number")
+	)
+	flag.Parse()
+
+	if *host == "" {
+		fmt.Fprintln(os.Stderr, fmt.Errorf("host is required"))
+		os.Exit(1)
 	}
-	s := server.NewServer(cfg)
-	s.Start()
+	stats, err := getStats(fmt.Sprintf("%s:%d", *host, *port))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	jsonString, err := json.Marshal(stats)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Println(string(jsonString))
+}
+
+func getStats(host string) (*minepong.Pong, error) {
+	conn, err := net.Dial("tcp", host)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+	stats, err := minepong.Ping(conn, host)
+	if err != nil {
+		return nil, err
+	}
+	return stats, nil
 }
